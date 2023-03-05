3f7c3113435aff55f870bb159fb8a6a7d0100b98
Erik Hollensbe <git@hollensbe.org>
2023-03-05 07:03:57 -0800

    commented things so I don't go insane later
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- pkg/dnsserver/dnsserver.go
+++ pkg/dnsserver/dnsserver.go
@@ -19,6 +19,9 @@ func (ds *DNSServer) Listen(listenSpec string) error {
 
 func (ds *DNSServer) findZone(question dns.Question) *config.Zone {
 	names := dns.SplitDomainName(question.Name)
+	// perform a greedy reverse search of the FQDN. If this code is working
+	// right, the longest match will be found first, finding the most local zone.
+	// This is probably broken in some subtle way, but YOLO.
 	for i := len(names); i >= 1; i-- {
 		potentialZone := strings.Join(names[len(names)-i-1:len(names)-1], ".")
 		if zone, ok := ds.Zones[potentialZone]; ok {
@@ -39,6 +42,9 @@ func (ds *DNSServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 		// NOTE: according to the docs for Questions, practically, only the first question matters
 		zone := ds.findZone(r.Question[0])
 		switch r.Question[0].Qtype {
+		// SOA and NS records are special in our implementation; they always
+		// exist just for the zone, and in a specific format. No need for a
+		// Match() call here.
 		case dns.TypeSOA:
 			answers = append(answers, zone.SOA.Convert().(dns.RR))
 		case dns.TypeNS:
@@ -58,6 +64,7 @@ func (ds *DNSServer) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	}
 
 	m.Authoritative = true
+	// NOTE we should verify if this setting is absolutely necessary, I cargo culted it from some old code.
 	m.RecursionAvailable = true
 	m.Answer = answers
 
