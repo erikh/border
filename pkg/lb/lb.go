6a31d148c81a9d65ec9ce68f4be9c14421d3712d
Erik Hollensbe <git@hollensbe.org>
2023-03-15 23:55:21 -0700

    move TCP balancer to own file
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- pkg/lb/lb.go
+++ pkg/lb/lb.go
@@ -2,10 +2,7 @@ package lb
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"io"
-	"log"
 	"net"
 	"sync"
 	"time"
@@ -96,38 +93,6 @@ func (b *Balancer) Shutdown() {
 	b.cancelFunc()
 }
 
-func (b *Balancer) BalanceTCP(ctx context.Context, notifyFunc func(error)) {
-	go b.monitorListen(ctx)
-	go b.dispatchTCP(ctx)
-	notifyFunc(nil)
-}
-
-func (b *Balancer) monitorListen(ctx context.Context) {
-	<-ctx.Done()
-	b.listener.Close()
-}
-
-func (b *Balancer) dispatchTCP(ctx context.Context) {
-	connChan := make(chan net.Conn, b.connBuffer)
-
-	go b.acceptConns(connChan)
-	go b.forwardConn(ctx, connChan)
-}
-
-func (b *Balancer) acceptConns(connChan chan net.Conn) {
-	for {
-		conn, err := b.listener.Accept()
-		if err != nil && !errors.Is(err, net.ErrClosed) {
-			log.Fatalf("Transient error in Accept, terminating listen. Restart border: %v", err)
-			return
-		} else if err != nil {
-			return
-		}
-
-		connChan <- conn
-	}
-}
-
 func (b *Balancer) closeConn(ctx context.Context, conn net.Conn) {
 	select {
 	case <-ctx.Done():
@@ -148,82 +113,7 @@ func (b *Balancer) decrementCount(ctx context.Context, lowestAddr string) {
 	b.mutex.Unlock()
 }
 
-func (b *Balancer) forwardConn(ctx context.Context, connChan chan net.Conn) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case conn := <-connChan:
-			var (
-				connCtx context.Context
-				cancel  context.CancelFunc
-			)
-
-			if b.timeout != 0 {
-				connCtx, cancel = context.WithTimeout(ctx, b.timeout)
-			} else {
-				connCtx, cancel = context.WithCancel(ctx)
-			}
-
-			go b.closeConn(connCtx, conn)
-
-			// find the lowest count of conns in the group. If all hosts are
-			// saturated, loop until that changes.
-		retry:
-			var lowestAddr string
-			var lowestCount uint64
-
-			b.mutex.RLock()
-			for addr := range b.backendAddresses {
-				count := b.backendConns[addr]
-				if lowestAddr == "" && count < b.maxConns {
-					lowestAddr = addr
-					lowestCount = count
-				} else if count < b.maxConns && count <= lowestCount {
-					lowestAddr = addr
-					lowestCount = count
-				}
-			}
-			b.mutex.RUnlock()
-
-			// if we have a designated lowest count address, dial the backend and
-			// schedule the copy. Remove the backend from the pool on any error.
-			if lowestAddr != "" {
-				b.mutex.Lock()
-				b.backendConns[lowestAddr]++
-				backend, err := net.Dial("tcp", lowestAddr)
-
-				// FIXME pool removal should happen as a result of health checks,
-				// not dial errors. We should try with a different address on dial
-				// errors, which we do by pruning the pool right now. Something
-				// more elegant should be employed in the face of transient errors.
-				if err != nil {
-					log.Printf("Backend %q failed: removing from pool: %v", lowestAddr, err)
-					delete(b.backendAddresses, lowestAddr)
-					delete(b.backendConns, lowestAddr)
-					b.mutex.Unlock()
-					goto retry
-				}
-
-				go b.closeConn(connCtx, backend)
-				go b.decrementCount(connCtx, lowestAddr)
-
-				// FIXME timeouts to prevent slowloris attacks. Also shutdown socket on context finish.
-				// FIXME probably should use CopyN to avoid other styles of slowloris attack (endless data)
-				go func() {
-					io.Copy(backend, conn)
-					cancel()
-				}()
-
-				go func() {
-					io.Copy(conn, backend)
-					cancel()
-				}()
-
-				b.mutex.Unlock()
-			} else {
-				goto retry
-			}
-		}
-	}
+func (b *Balancer) monitorListen(ctx context.Context) {
+	<-ctx.Done()
+	b.listener.Close()
 }
