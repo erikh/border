309649c8f519d4e7d647bff1aa608b78cc24fe59
Erik Hollensbe <git@hollensbe.org>
2023-03-26 02:10:34 -0700

    commented the heck out of the election test
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- pkg/controlserver/election_test.go
+++ pkg/controlserver/election_test.go
@@ -86,13 +86,26 @@ func TestElection(t *testing.T) {
 		index              uint
 	)
 
+	// this loop is pretty hairy, so I've attempted to over-comment it.
 	for _, peer := range c.Peers {
 		client := makeClient(peer.ControlServer, peer.Key)
+
+		// starting an election on all peers will cause it to agree on a
+		// electorate, or where the votes will be submitted, based on the uptime of
+		// all the peers in nanoseconds. The highest uptime wins.
 		resp, err := client.Exchange(&api.StartElectionRequest{}, true)
+
+		// if the election is incomplete, it's fine, we just have to take some
+		// special cases. This is because the first peer was not the electorate, so
+		// later on, we have to start a vote prematurely. On the second iteration,
+		// the "start election" code complains that one is already in progress.
 		if err != nil && !strings.Contains(err.Error(), ErrElectionIncomplete.Error()) {
 			t.Fatal(err)
 		}
 
+		// if we get an error up there, resp will be nil, so don't try to calculate
+		// the electorate. Because of the condition to get here, it's already been
+		// picked anyway.
 		if resp != nil {
 			ep := resp.(*api.StartElectionResponse).ElectoratePeer
 			idx := resp.(*api.StartElectionResponse).Index
@@ -116,30 +129,38 @@ func TestElection(t *testing.T) {
 
 		electorateClient := makeClient(electoratePeer.ControlServer, electoratePeer.Key)
 
+		// an invalid election index should always fail.
 		_, err = electorateClient.Exchange(&api.ElectionVoteRequest{Index: 0, Me: peer.Name(), Peer: peer.Name()}, true)
 		if err == nil {
 			t.Fatal("accepted vote when shouldn't have: invalid index")
 		}
 
+		// if we aren't the electorate:
 		if electoratePeerName != peer.Name() {
+			// start an election with the electorate already picked. this is why the
+			// special casing around starting an election above is necessary. No
+			// votes will work until the election has started.
 			electorateClient.Exchange(&api.StartElectionRequest{}, true)
 
+			// try to register a vote on the peer, which should fail, because it's
+			// not the electorate.
 			_, err = client.Exchange(&api.ElectionVoteRequest{Index: index, Me: peer.Name(), Peer: peer.Name()}, true)
 			if err == nil {
 				t.Fatal("accepted vote when shouldn't have: invalid electorate")
 			}
 
+			// try to identify the publisher, again, not the electorate
 			_, err = client.Exchange(&api.IdentifyPublisherRequest{}, true)
 			if err == nil {
 				t.Fatal("Invalid identification of publisher")
 			}
 		}
 
+		// finally, elect someone
 		_, err = electorateClient.Exchange(&api.ElectionVoteRequest{Index: index, Me: peer.Name(), Peer: electoratePeerName}, true)
 		if err != nil {
 			t.Fatal(err)
 		}
-
 	}
 
 	electoratePeer, err := c.FindPeer(electoratePeerName)
@@ -148,11 +169,15 @@ func TestElection(t *testing.T) {
 	}
 
 	electorateClient := makeClient(electoratePeer.ControlServer, electoratePeer.Key)
+
+	// assuming all is well, identifying the publisher should work at this point,
+	// which will just be the electoratePeer because that's who we voted for.
 	resp, err := electorateClient.Exchange(&api.IdentifyPublisherRequest{}, true)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// validate the parameters to ensure nothing is racing / out of whack.
 	ipr := resp.(*api.IdentifyPublisherResponse)
 
 	if ipr.EstablishedIndex != index {
