e6f146926ed8dfdd52e8d68e771ed23ad1858d6f
Erik Hollensbe <git@hollensbe.org>
2023-03-30 17:48:07 -0700

    file organization issues
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/erikh/border/pkg/config"
 )
@@ -176,82 +175,4 @@ func (crr *ConfigReloadRequest) SetNonce(nonce []byte) error {
 
 func (crr *ConfigReloadRequest) Marshal() ([]byte, error) {
 	return json.Marshal(crr)
-}
-
-type UptimeRequest struct {
-	NonceValue []byte `json:"nonce"`
-}
-
-func (*UptimeRequest) New() Request {
-	return &UptimeRequest{}
-}
-
-func (ur *UptimeRequest) Response() Message {
-	return &UptimeResponse{}
-}
-
-func (ur *UptimeRequest) Endpoint() string {
-	return PathUptime
-}
-
-func (ur *UptimeRequest) Unmarshal(byt []byte) error {
-	return json.Unmarshal(byt, ur)
-}
-
-func (ur *UptimeRequest) Nonce() string {
-	return string(ur.NonceValue)
-}
-
-func (ur *UptimeRequest) SetNonce(nonce []byte) error {
-	ur.NonceValue = nonce
-	return nil
-}
-
-func (ur *UptimeRequest) Marshal() ([]byte, error) {
-	return json.Marshal(ur)
-}
-
-type UptimeResponse struct {
-	Uptime time.Duration `json:"uptime"`
-}
-
-func (ur *UptimeResponse) Unmarshal(byt []byte) error {
-	return json.Unmarshal(byt, ur)
-}
-
-func (ur *UptimeResponse) Marshal() ([]byte, error) {
-	return json.Marshal(ur)
-}
-
-type PingRequest struct {
-	NonceValue []byte `json:"nonce"`
-}
-
-func (*PingRequest) New() Request {
-	return &PingRequest{}
-}
-
-func (*PingRequest) Response() Message {
-	return &NilResponse{}
-}
-
-func (*PingRequest) Endpoint() string {
-	return PathPing
-}
-
-func (pr *PingRequest) Unmarshal(byt []byte) error {
-	return json.Unmarshal(byt, pr)
-}
-
-func (pr *PingRequest) Nonce() string {
-	return string(pr.NonceValue)
-}
-
-func (pr *PingRequest) SetNonce(nonce []byte) error {
-	pr.NonceValue = nonce
-	return nil
-}
-
-func (pr *PingRequest) Marshal() ([]byte, error) {
-	return json.Marshal(pr)
 }
--- pkg/api/peer.go
+++ pkg/api/peer.go
@@ -1,5 +1,10 @@
 package api
 
+import (
+	"encoding/json"
+	"time"
+)
+
 const (
 	PathPeerNonce = "peerNonce"
 	PathUptime    = "uptime"
@@ -34,4 +39,82 @@ func (*PeerNonceRequest) SetNonce(nonce []byte) error {
 
 func (*PeerNonceRequest) Marshal() ([]byte, error) {
 	return []byte("{}"), nil
+}
+
+type UptimeRequest struct {
+	NonceValue []byte `json:"nonce"`
+}
+
+func (*UptimeRequest) New() Request {
+	return &UptimeRequest{}
+}
+
+func (ur *UptimeRequest) Response() Message {
+	return &UptimeResponse{}
+}
+
+func (ur *UptimeRequest) Endpoint() string {
+	return PathUptime
+}
+
+func (ur *UptimeRequest) Unmarshal(byt []byte) error {
+	return json.Unmarshal(byt, ur)
+}
+
+func (ur *UptimeRequest) Nonce() string {
+	return string(ur.NonceValue)
+}
+
+func (ur *UptimeRequest) SetNonce(nonce []byte) error {
+	ur.NonceValue = nonce
+	return nil
+}
+
+func (ur *UptimeRequest) Marshal() ([]byte, error) {
+	return json.Marshal(ur)
+}
+
+type UptimeResponse struct {
+	Uptime time.Duration `json:"uptime"`
+}
+
+func (ur *UptimeResponse) Unmarshal(byt []byte) error {
+	return json.Unmarshal(byt, ur)
+}
+
+func (ur *UptimeResponse) Marshal() ([]byte, error) {
+	return json.Marshal(ur)
+}
+
+type PingRequest struct {
+	NonceValue []byte `json:"nonce"`
+}
+
+func (*PingRequest) New() Request {
+	return &PingRequest{}
+}
+
+func (*PingRequest) Response() Message {
+	return &NilResponse{}
+}
+
+func (*PingRequest) Endpoint() string {
+	return PathPing
+}
+
+func (pr *PingRequest) Unmarshal(byt []byte) error {
+	return json.Unmarshal(byt, pr)
+}
+
+func (pr *PingRequest) Nonce() string {
+	return string(pr.NonceValue)
+}
+
+func (pr *PingRequest) SetNonce(nonce []byte) error {
+	pr.NonceValue = nonce
+	return nil
+}
+
+func (pr *PingRequest) Marshal() ([]byte, error) {
+	return json.Marshal(pr)
 }
