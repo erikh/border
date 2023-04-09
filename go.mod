97e56f78bc54d8b287f741bc2da65f6270767b90
Erik Hollensbe <git@hollensbe.org>
2023-04-08 20:33:43 -0700

    ACME: First stab at an account creator & certificate obtainer
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- go.mod
+++ go.mod
@@ -10,10 +10,12 @@ require (
 	github.com/fsouza/go-dockerclient v1.9.7
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-jose/go-jose/v3 v3.0.0
+	github.com/mholt/acmez v1.1.0
 	github.com/miekg/dns v1.1.53
 	github.com/peterbourgon/ff v1.7.1
 	github.com/sirupsen/logrus v1.9.0
 	github.com/vishvananda/netlink v1.2.1-beta.2
+	go.uber.org/zap v1.24.0
 	golang.org/x/sys v0.7.0
 )
 
@@ -35,9 +37,12 @@ require (
 	github.com/opencontainers/runc v1.1.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/vishvananda/netns v0.0.0-20210104183010-2eb08e3e575f // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/tools v0.8.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ pkg/acmekit/acmekit.go
@@ -0,0 +1,144 @@
+package acmekit
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/mholt/acmez"
+	"github.com/mholt/acmez/acme"
+	"go.uber.org/zap"
+)
+
+type Certificate struct {
+	Chain      []byte `json:"chain"`
+	PrivateKey []byte `json:"private_key"`
+}
+
+type Account struct {
+	Information  acme.Account            `json:"info"`
+	PrivateKey   []byte                  `json:"private_key"`
+	Certificates map[string]*Certificate `json:"certificates"`
+}
+
+type ACMEParams struct {
+	Account      *Account `json:"account"`
+	IgnoreVerify bool     `json:"ignore_verify"`
+	Directory    *url.URL `json:"acme_directory"`
+	ContactInfo  []string `json:"contact_info"`
+}
+
+func getZapLogger() (*zap.Logger, error) {
+	return zap.NewDevelopment()
+}
+
+func generatePrivateKey() (*ecdsa.PrivateKey, []byte, error) {
+	pkey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Could not generate private key for ACME after generation: %w", err)
+	}
+
+	marshalledPKey, err := x509.MarshalECPrivateKey(pkey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Could not marshal private key for ACME after generation: %w", err)
+	}
+
+	return pkey, marshalledPKey, nil
+}
+
+// Determine if the user has an already properly registered and active account
+// with the ACME server.
+func (ap *ACMEParams) HasValidAccount() bool {
+	return ap.Account != nil && ap.Account.Information.Status == "valid"
+}
+
+// makeClient makes an ACME client
+func (ap *ACMEParams) makeClient() (*acmez.Client, error) {
+	logger, err := getZapLogger()
+	if err != nil {
+		return nil, fmt.Errorf("Could not create logger: %w", err)
+	}
+
+	return &acmez.Client{
+		Client: &acme.Client{
+			Directory: ap.Directory.String(),
+			HTTPClient: &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: ap.IgnoreVerify,
+					},
+				},
+			},
+			Logger: logger,
+		},
+	}, nil
+}
+
+// CreateAccount creates a private key, attempts to create an account with the
+// ACME server, and returns no error if that was successful, filling the
+// properties in the Account member if so. If the Account field is already
+// populated, it will overwrite those values with new ones. Use HasValidAccount
+// to determine if the account is already set.
+func (ap *ACMEParams) CreateAccount(ctx context.Context) error {
+	pkey, marshalledPKey, err := generatePrivateKey()
+	if err != nil {
+		return err
+	}
+
+	account := acme.Account{
+		Contact:              ap.ContactInfo,
+		TermsOfServiceAgreed: true,
+		PrivateKey:           pkey,
+	}
+
+	client, err := ap.makeClient()
+	if err != nil {
+		return fmt.Errorf("Error while making ACME client: %w", err)
+	}
+
+	account, err = client.NewAccount(ctx, account)
+	if err != nil {
+		return fmt.Errorf("Could not create new ACME account: %w", err)
+	}
+
+	ap.Account = &Account{
+		Information: account,
+		PrivateKey:  marshalledPKey,
+	}
+
+	return nil
+}
+
+func (ap *ACMEParams) GetCertificate(ctx context.Context, domain string) error {
+	pkey, marshalledPKey, err := generatePrivateKey()
+	if err != nil {
+		return err
+	}
+
+	client, err := ap.makeClient()
+	if err != nil {
+		return fmt.Errorf("Error while making ACME client: %w", err)
+	}
+
+	certs, err := client.ObtainCertificate(ctx, ap.Account.Information, pkey, []string{domain})
+	if err != nil {
+		return fmt.Errorf("Error obtaining certificate from ACME directory: %w", err)
+	}
+
+	if ap.Account.Certificates == nil {
+		ap.Account.Certificates = map[string]*Certificate{}
+	}
+
+	ap.Account.Certificates[domain] = &Certificate{
+		Chain:      certs[0].ChainPEM,
+		PrivateKey: marshalledPKey,
+	}
+
+	return nil
+}
